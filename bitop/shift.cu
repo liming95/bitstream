4bac7a1134704fea084ed8ddad69deabd311351f
liming95 <951238600@qq.com>
2025-04-01 00:09:21 +0800

    update: shift function

--- bitop/shift.cu
+++ bitop/shift.cu
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <cuda_runtime.h>
 
-#define SET_HIGH_BIT(number, length) (number | (1 << (length-1)))
+#define SET_HIGH_BIT(number, size) (number | (1 << (size-1)))
+#define CLEAR_HIGH_BIT(number, size) (number ^ (1 << (size-1)))
 #define CLEAR_LOW_BIT(number) (number ^ 1)
-#define grid_size 2
+#define WAIT(number, size, condition) while((__ldg(&number) >> (size-1)) != condition)
+#define grid_size 1
 #define block_size 2
 #define thread_size 2
 
@@ -13,32 +16,50 @@
  * shift_global: shift in and shift out bit
  */
 __global__ void shift_left(uint32_t *bit_stream, int size, int shift_count, uint32_t* shift_global){
-    __shared__ shift_block[thread_size];
-    //block inner
+    __shared__ uint32_t shift_block[thread_size];
     int thread_idx = threadIdx.x + blockIdx.x * blockDim.x;
-    uint32_t stream = bit_stream[thread_idx];
+    printf("thread[%u,%u,(%u, %u, %u)]: bit_stream[%d] = %u\n", blockIdx.x, threadIdx.x,
+        blockDim.x, blockDim.y, blockDim.z, thread_idx, bit_stream[thread_idx]);
+    // uint32_t stream = bit_stream[thread_idx];
     if(threadIdx.x == 0 & blockIdx.x == 0){
+        //device inter
         shift_block[0] |= shift_global[0];
+        __threadfence();
+        shift_global[0] = CLEAR_HIGH_BIT(shift_global[0], sizeof(uint32_t));
     }
 
     if(threadIdx.x < blockDim.x-1){
+        //block inner
         shift_block[threadIdx.x+1] = bit_stream[thread_idx] >> (size - shift_count);
+        __threadfence_block();
+        shift_block[threadIdx.x+1] = SET_HIGH_BIT(shift_block[threadIdx.x+1], sizeof(uint32_t));
     }
     else{
-        shift_global[(blockIdx.x+1)%gridDim.x] |= bit_stream[thread_idx] >> (size - shift_count);
+        //block inter & device inter
+        int block_idx = (blockIdx.x + 1) % gridDim.x;
+        WAIT(shift_global[block_idx], sizeof(uint32_t), 0);
+        shift_global[block_idx] |= bit_stream[thread_idx] >> (size - shift_count);
+        __threadfence();
+        shift_global[block_idx] = SET_HIGH_BIT(shift_global[block_idx], sizeof(uint32_t));
     }
 
-    if(threadIdx.x == 0 & blockIdx.x != 0){
-        shift_block[0] = shift_blobal[blockIdx.x];
+    if(threadIdx.x == 0 && blockIdx.x != 0){
+        //block inter
+        WAIT(shift_global[blockIdx.x], sizeof(uint32_t), 1);
+        shift_block[0] = shift_block[blockIdx.x];
+        shift_block[blockIdx.x] = CLEAR_HIGH_BIT(shift_block[blockIdx.x], sizeof(uint32_t));
     }
 
-    bit_stream[thread_idx];
-    //block inter
+    WAIT(shift_block[threadIdx.x], sizeof(uint32_t), 1);
+    shift_block[threadIdx.x] = CLEAR_HIGH_BIT(shift_block[threadIdx.x], sizeof(uint32_t));
+    bit_stream[thread_idx] = (bit_stream[thread_idx] << shift_count) | shift_block[threadIdx.x];
+
+
 }
 
 void get_bit_stream(uint32_t *bit_stream, int size){
     for(int i = 0; i < size; i++){
-        bit_stream[i] = SET_HIGH_BIT(bit_steam[i], size);
+        bit_stream[i] = SET_HIGH_BIT(bit_stream[i], sizeof(uint32_t));
     }
 }
 
@@ -58,18 +79,17 @@ int main()
 
     cudaMalloc((void **)&bit_stream_gpu, sizeof(uint32_t)*grid_dim);
     cudaMalloc((void **)&shift_global_gpu, sizeof(uint32_t)*block_size);
-    *shift_global = 0;
 
-    cudaMemcpy(shift_global_gpu, shift_global, sizeof(uint32_t, cudaMemcpyHostToDevice));
+    shift_global[0] = SET_HIGH_BIT(shift_global[0], sizeof(uint32_t));
+    cudaMemcpy(shift_global_gpu, shift_global, sizeof(uint32_t)*block_size, cudaMemcpyHostToDevice);
     get_bit_stream(bit_stream, bit_stream_size);
-
-
 
     for(int i = 0; i < iteration_time; i++){
         cudaMemcpy(bit_stream_gpu, bit_stream+i*grid_dim,
             grid_dim*sizeof(uint32_t), cudaMemcpyHostToDevice);
 
-        shift_left<<<block_size, thread_size>>>(bit_stream_gpu, sizeof(uint32_t), shift_count, shift_global_gpu);
+        dim3 threadsPerBlock(thread_size,1,1);
+        shift_left<<<block_size, threadsPerBlock>>>(bit_stream_gpu, sizeof(uint32_t), shift_count, shift_global_gpu);
 
         cudaMemcpy(bit_stream+i*grid_dim, bit_stream_gpu,
             grid_dim*sizeof(uint32_t), cudaMemcpyDeviceToHost);
@@ -77,11 +97,11 @@ int main()
 
     for(int i = 0; i < bit_stream_size; i++){
         if(CLEAR_LOW_BIT(bit_stream[i]) == bit_stream_shift[i]){
-            printf("[%d]: {bit_stream:%u, shift_global_stream:%u", i,
+            printf("[%d]: {bit_stream:%u, shift_global_stream:%u}\n", i,
                 bit_stream[i] >> shift_count, bit_stream_shift[i]);
         }
         else{
-            printf("ERROR[%d]: {bit_stream:%u, shift_global_stream:%u}", i,
+            printf("ERROR[%d]: {bit_stream:%u, shift_global_stream:%u}\n", i,
                 bit_stream[i] >> shift_count, bit_stream_shift[i]);
         }
     }
@@ -90,5 +110,4 @@ int main()
     cudaFree(bit_stream_gpu);
 
     return 0;
-
 }
